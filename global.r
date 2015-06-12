c7794f74bafa474470af912768695365d088de07
Stefan Wojcik <stefan.wojcik@colorado.edu>
2015-06-12 10:30:23 -0400

    Create global.r

---
+++ global.r
@@ -0,0 +1,6 @@
+
+setwd("D:/papers/Working Projects/Dissertation/ShinyNetSurvey/NewApp/Code_writing_app")
+#runApp("D:/papers/Working Projects/Dissertation/ShinyNetSurvey/NewApp/Code_writing_app")
+initialuicode <- readLines("uicode.txt")
+initialservercode <- readLines("servercode.txt")
+initialglobalcode <- readLines("globalcode.txt")
